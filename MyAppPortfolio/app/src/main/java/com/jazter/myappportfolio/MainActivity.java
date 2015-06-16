627a97a8118973565c5af90462e762e20abd0a9e
Ulises Escobar <jazter_1@hotmail.com>
2015-06-15 19:31:46 -0500

    My App Portfolio finished

--- MyAppPortfolio/app/src/main/java/com/jazter/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/jazter/myappportfolio/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jazter.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +37,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonClicked(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch an app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
